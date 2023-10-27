66ad9d184d0b2f96fc7caa5ddd3866de708195fc
Sallys Carlos <sallys.carlos93@gmail.com>
2023-10-27 15:56:39 -0300

    chore: initial config

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Home from "./pages/Home";
+import { GlobalStyles } from "./styles/global";
+
+const App = () => {
+  return (
+    <>
+      <Home />
+      <GlobalStyles />
+    </>
+  );
+};
+
+export default App;
