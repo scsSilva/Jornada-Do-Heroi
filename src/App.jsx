4a51581acf465ce8036299c5223866aa1ffe2a19
Sallys Carlos <sallys.carlos93@gmail.com>
2023-10-27 17:12:52 -0300

    feat: wrapper app with context provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
+import { HeroesContextProvider } from "./contexts/HeroesContext";
 import Home from "./pages/Home";
 import { GlobalStyles } from "./styles/global";
 
 const App = () => {
   return (
-    <>
+    <HeroesContextProvider>
       <Home />
       <GlobalStyles />
-    </>
+    </HeroesContextProvider>
   );
 };
 
